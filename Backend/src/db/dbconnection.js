26901258336078dcca76bf8e127e9c96eb924efb
vinay961 <vinay100rai@gmail.com>
2024-12-28 10:00:19 +0530

    database connected sucessfully

--- Backend/src/db/dbconnection.js
+++ Backend/src/db/dbconnection.js
@@ -1,16 +1,16 @@
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
-dotenv.config() // load the env content in process.env by default
+dotenv.config(); // load the env content in process.env by default
 
 const connectDB = async() => {
     try {
-        const connection = await mongoose.connect(process.env.MONGO_URI,{dbName:process.env.DB_NAME,tlsInsecure: true})
-        console.log(`\n MongoDB connected !! DB HOST: ${connection.host}`);
+        const connection = await mongoose.connect(process.env.MONGO_URI, {dbName:process.env.DB_NAME});
+        console.log(`\n MongoDB connected !! DB HOST: ${connection.connection.host}`);
     } catch (error) {
         console.log(error);
         console.log('DB connection failed.');
-        process.exit(1)
+        process.exit(1);
     }
 }
 
